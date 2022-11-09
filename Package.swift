f01e7ec3b54611a6e0c76a11456b0fe45115293a
Alan Charles <acharles@twilio.com>
2022-11-09 14:15:25 -0700

    feat: add flurry destination plugin

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SegmentDestination",
+    name: "SegmentFlurry",
     platforms: [
         .iOS("13.0"),
         .tvOS("11.0"),
@@ -13,8 +13,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SegmentDestination",
-            targets: ["SegmentDestination"]),
+            name: "SegmentFlurry",
+            targets: ["SegmentFlurry"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,14 +23,19 @@ let package = Package(
             name: "Segment",
             url: "https://github.com/segmentio/analytics-swift.git",
             from: "1.1.2"
-        )
+        ),
+        .package(
+            name: "Flurry",
+            url:  "https://github.com/flurry/FlurrySwiftPackage/",
+            from: "12.0.0"
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SegmentDestination",
-            dependencies: ["Segment"]),
+            name: "SegmentFlurry",
+            dependencies: ["Segment", "Flurry"]),
         
         // TESTS ARE HANDLED VIA THE EXAMPLE APP.
     ]
